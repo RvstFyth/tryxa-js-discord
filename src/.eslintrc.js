043db68931c551ffeeeed2d74dd958bdfed2380b
RvStFyth <r.vansintfeyth@protonmail.com>
2021-11-09 20:49:29 +0100

    eslint config file

---
+++ src/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "browser": false,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 13
+    },
+    "rules": {
+    }
+};
