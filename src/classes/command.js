11b86482e4b72667ba99ff915efb312e8f3f8f53
RvStFyth <r.vansintfeyth@protonmail.com>
2021-11-09 20:47:34 +0100

    Added a askToConfirm method to command class

--- src/classes/command.js
+++ src/classes/command.js
@@ -1,3 +1,5 @@
+const emojiHelper = require('../helpers/emojis');
+
 class Command
 {
 
@@ -9,6 +11,30 @@ class Command
     setArguments(args)
     {
         this.arguments = args;
+    }
+
+    async askToConfirm(description, removeAfterTimeout = false)
+    {
+        return new Promise(resolve => {
+            const embed = {
+                title: `Confirm`,
+                description
+            };
+
+            this.message.channel.send({embeds: [embed]}).then(async msg => {
+                await msg.react(emojiHelper.baseEmojis.confirm);
+                const filter = (reaction, user) => reaction.emoji.name === emojiHelper.baseEmojis.confirm;// && user.id === this.message.author.id;
+
+                msg.awaitReactions({filter, max: 1, time: 10*1000}).then(async collected => {
+                    const reaction = collected.first();
+                    if(reaction) resolve(true);
+                    else {
+                        if(removeAfterTimeout) await msg.delete();
+                        resolve(false);
+                    }
+                })
+            }).catch(e => console.log(e));
+        });
     }
 }
 
