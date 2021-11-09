a5ed2eea5e0b536f1a1f87b24b1b8f0591fdc776
RvStFyth <r.vansintfeyth@protonmail.com>
2021-11-09 20:47:51 +0100

    Skeleton start command file

--- src/commands/start.js
+++ src/commands/start.js
@@ -1,4 +1,5 @@
 const Command = require('../classes/command');
+const usersModel = require('../models/users');
 
 class Start extends Command
 {
@@ -9,7 +10,18 @@ class Start extends Command
 
     async run()
     {
-        this.message.channel.send({content:`Start!`});
+        const existingCharacter = await usersModel.getForDiscordID(this.message.author.id);
+        if(existingCharacter) {
+            return this.message.channel.send({content:`**${this.message.author.username}** you already have a account!`});
+        }
+
+        if(!this.arguments.length) {
+            return this.message.channel.send({content:`**${this.message.author.username}**, please provide a name for your character..`});
+        }
+
+        const confirmed = await this.askToConfirm(`**${this.message.author.username}**, are you sure you want to name your character: ${this.arguments[0]}?`, true);
+        if(confirmed) this.message.channel.send({content:`YES!`});
+        else this.message.channel.send({content:`NO!`});
     }
 }
 
