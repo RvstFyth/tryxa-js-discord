5247b1e676fe44308da1469896ecf1e83b6ba5d0
RvStFyth <r.vansintfeyth@protonmail.com>
2021-11-13 18:21:54 +0100

    Command for testing loot generator

--- src/commands/roll.js
+++ src/commands/roll.js
@@ -11,11 +11,21 @@ class Roll extends Command
         if (this.arguments[0] && !isNaN(this.arguments[0])) level = parseInt(this.arguments[0]);
         const tier = parseInt(level / 10);
 
+        const slots = ['head', 'body', 'hands', 'legs', 'weapon', 'offhand', 'lfinger', 'rfinger'];
+        const slot = random.arrayValue(slots);
+
         const stats = {
-            strength: 0, intelligence: 0, wisdom: 0, dexterity: 0, constitution: 0, luck: 0
+            strength: 0, intelligence: 0, wisdom: 0, dexterity: 0, constitution: 0,
         };
 
-        for(let i = 0; i <= level; i++) { // levels + 1 = amount of stats point applied
+        const rarities = ['common', 'uncommon', 'rare', 'epic', 'LEGENDARY'];
+        const rarity = random.weightedRandom(1, rarities.length);
+
+        if(['lfinger', 'rfinger'].indexOf(slot) > -1)  stats['luck'] = 0;
+
+        const maxStats = level + rarity;
+
+        for(let i = 0; i <= maxStats; i++) { // levels + 1 = amount of stats point applied
             const randomStat = random.arrayValue(Object.keys(stats));
             stats[randomStat]++;
             if(random.number(1, 100) === 3) {
@@ -24,7 +34,7 @@ class Roll extends Command
             }
         }
 
-        return this.message.channel.send({content: `Level: ${level}\nTier: ${tier}\n${JSON.stringify(stats)}`});
+        return this.message.channel.send({content: `Level: ${level}\nTier: ${tier}\nSlot: ${slot}\nRarity: ${rarities[rarity - 1]}\n${JSON.stringify(stats)}`});
     }
 }
 
