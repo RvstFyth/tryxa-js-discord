604eda8510d6da059c9af525017f8e784b4f9d65
RvStFyth <r.vansintfeyth@protonmail.com>
2021-11-13 15:50:04 +0100

    Start command now uses translations

--- src/commands/start.js
+++ src/commands/start.js
@@ -12,18 +12,18 @@ class Start extends Command
     async run(character)
     {
         if(character) {
-            return this.message.channel.send({content:`**${this.message.author.username}** you already have a account!`});
+            return this.message.channel.send({content:`**${this.message.author.username}** ${this.getTranslation('commands/start', 'alreadyRegistered')}`});
         }
 
         if(!this.arguments.length) {
-            return this.message.channel.send({content:`**${this.message.author.username}**, please provide a name for your character..`});
+            return this.message.channel.send({content:`**${this.message.author.username}**, ${this.getTranslation('commands/start', 'noNameProvided')}`});
         }
 
-        const confirmed = await this.askToConfirm(`**${this.message.author.username}**, are you sure you want to name your character: ${this.arguments[0]}?`, true);
+        const confirmed = await this.askToConfirm(`**${this.message.author.username}**, ${this.getTranslation('commands/start', 'confirm', {arg: this.arguments[0]})}`, true);
         if(confirmed) {
             const userID = await usersModel.create(this.message.author.id, this.arguments[0]);
             await userStatsModel.create(userID);
-            await this.message.reply(`Welcome to **Tryxa** ${this.message.author.username}! See the \`help\` files to get started!`);
+            await this.message.reply(`${this.getTranslation('commands/start', 'registered', {username: this.message.author.username})}`);
         }
     }
 }
