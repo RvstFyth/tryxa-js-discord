ad5e06e657a95a09343f5ab105ac0ad7013aa75a
RvStFyth <r.vansintfeyth@protonmail.com>
2021-11-13 11:10:46 +0100

    Command to check what items are equipped

---
+++ src/commands/equipped.js
@@ -0,0 +1,25 @@
+const Command = require('../classes/command');
+
+class Equipped extends Command {
+
+    async run(character)
+    {
+        const fields = [];
+
+        for(let i in character.equipped) {
+            fields.push({
+                name: i,
+                value: `X`,
+                inline: true
+            });
+        }
+        const embed = {
+            title: `${character.name} equipped`,
+            fields: fields
+        };
+
+        return this.message.channel.send({embeds: [embed]});
+    }
+}
+
+module.exports = Equipped;
