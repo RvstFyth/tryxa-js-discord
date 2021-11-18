0e125a6dc9ba339ed2bd1d0697d9444f8bee566e
RvStFyth <r.vansintfeyth@protonmail.com>
2021-11-18 19:05:56 +0100

    Migration file for user moves table

---
+++ src/migrations/20211118180156-create-user-moves-table.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return db.createTable('user_moves', {
+    id: {type: 'int', primaryKey: true, autoIncrement: true},
+    user_id: {type: 'int', defaultValue: null},
+    move: {type: 'string', defaultValue: ''},
+    learned: {type: 'integer', defaultValue: 5},
+    active: {type: 'boolean', defaultValue: false}
+  });
+};
+
+exports.down = function(db) {
+  return db.dropTable('user_moves');
+};
+
+exports._meta = {
+  "version": 1
+};
