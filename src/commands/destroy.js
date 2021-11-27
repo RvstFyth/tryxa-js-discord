dc2994ab329e9f80beac4a89969e1b5b77b083f0
RvStFyth <r.vansintfeyth@protonmail.com>
2021-11-27 15:16:48 +0100

    prevent players destroying equipped items

--- src/commands/destroy.js
+++ src/commands/destroy.js
@@ -9,6 +9,7 @@ class Destroy extends Command
         if(this.arguments[0]) {
             const item = await itemsModel.getForUser(this.arguments[0], character.id);
             if(item) {
+                if(character.getEquippedIDs().indexOf(item.id) > -1) return this.message.channel.send(`**${character.name}** you can't destroy equipped items..`);
                 await itemsModel.delete(item.id);
                 return this.message.channel.send(`**${character.name}** destroyed a level ${item.level} ${item.name}`);
             }
