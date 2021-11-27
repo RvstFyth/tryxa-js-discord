0661eabd6275e592d0359db5d9f99f97fea469ff
RvStFyth <r.vansintfeyth@protonmail.com>
2021-11-27 12:58:11 +0100

    kinda working version for kick move

--- src/classes/moves/kick.js
+++ src/classes/moves/kick.js
@@ -1,4 +1,5 @@
 const Move = require('../move');
+const random = require('../../helpers/random');
 
 class Kick extends Move
 {
@@ -15,7 +16,9 @@ class Kick extends Move
     {
         const target = targets[0];
         if(target) {
-
+            const dmg = random.number(1, this.character.stats.strength);
+            target.stats.health -= dmg;
+            return `${this.character.name} kicked ${targets[0].name} and did ${dmg} damage`;
         }
         else {
             // TODO:
