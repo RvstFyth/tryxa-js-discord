6dea9b432c1b5be37d37a907f02b2a960650de2b
RvStFyth <r.vansintfeyth@protonmail.com>
2021-11-27 14:51:02 +0100

    Method to roll random items

--- src/commands/roll.js
+++ src/commands/roll.js
@@ -1,59 +1,30 @@
 const itemsModel = require('../models/items');
 const Command = require('../classes/command');
 const random = require('../helpers/random');
+const itemsHelper = require('../helpers/items');
 
 class Roll extends Command
 {
 
     async run(character)
     {
-        let level = 1;
-        if (this.arguments[0] && !isNaN(this.arguments[0])) level = parseInt(this.arguments[0]);
-        const tier = parseInt(level / 10);
+        const item = await itemsHelper.rollItem(character.id, character.level);
 
-        const slots = ['head', 'body', 'hands', 'legs', 'weapon', 'offhand', 'finger', 'finger'];
-        const slot = random.arrayValue(slots);
+        let statsString = '';
+        for(let i in item.stats) {
+            if(i) statsString += `${i}: ${item.stats[i]}, `
+        }
 
-        const stats = {
-            strength: 0, intelligence: 0, wisdom: 0, dexterity: 0, constitution: 0,
+        const embed = {
+            title: `${character.name} item roll`,
+            description: `` +
+                `Name: ${item.name}\n` +
+                `Level: ${item.level}\n` +
+                `Stats: ${statsString}\n`+
+                `Rarity: ${itemsHelper.rarityMapping[item.rarity - 1]}`
         };
 
-        const rarities = ['common', 'uncommon', 'rare', 'epic', 'LEGENDARY'];
-        const rarity = random.weightedRandom(1, rarities.length);
-
-        if(['finger', 'finger'].indexOf(slot) > -1)  stats['luck'] = 0;
-
-        const maxStats = level + rarity;
-
-        let name;
-        switch (slot) {
-            case 'finger': name = 'ring'; break;
-            case 'body': name = 'armor'; break;
-            case 'head': name = 'helmet'; break;
-            case 'hands': name = 'gloves'; break;
-            case 'legs': name = 'leggings'; break;
-            case 'weapon': name = 'sword'; break;
-            case 'offhand': name = 'shield'; break;
-        }
-
-        for(let i = 0; i <= maxStats; i++) { // levels + 1 = amount of stats point applied
-            const randomStat = random.arrayValue(Object.keys(stats));
-            stats[randomStat]++;
-            if(random.number(1, 100) === 3) {
-                const randomStat = random.arrayValue(Object.keys(stats));
-                stats[randomStat]++;
-            }
-        }
-
-        const finalStats = {};
-        let statsString = '';
-        for(let i in stats) {
-            if(stats[i] > 0) finalStats[i] = stats[i];
-            statsString += `;${i}:${stats[i]}`
-        }
-
-        await itemsModel.create(character.id, name, rarity, slot, statsString, level);
-        return this.message.channel.send({content: `Name: ${name}\nLevel: ${level}\nTier: ${tier}\nSlot: ${slot}\nRarity: ${rarities[rarity - 1]}\n${JSON.stringify(finalStats)}`});
+        return this.message.channel.send({embeds: [embed]});
     }
 }
 
--- src/helpers/items.js
+++ src/helpers/items.js
@@ -1,3 +1,7 @@
+const random = require("../helpers/random");
+const itemsModel = require("../models/items");
+const Item = require('../classes/item');
+
 module.exports = {
 
     rarityMapping: ['common', 'uncommon', 'rare', 'epic', 'LEGENDARY'],
@@ -15,5 +19,59 @@ module.exports = {
             'crappy',
             'tin'
         ]
+    },
+
+    composeNameForSlotName(slot, tier) {
+
+    },
+
+    async rollItem(characterID, level)
+    {
+        const tier = parseInt(level / 10);
+
+        const slots = ['head', 'body', 'hands', 'legs', 'weapon', 'offhand', 'finger', 'finger'];
+        const slot = random.arrayValue(slots);
+
+        const stats = {
+            strength: 0, intelligence: 0, wisdom: 0, dexterity: 0, constitution: 0,
+        };
+
+        const rarities = ['common', 'uncommon', 'rare', 'epic', 'LEGENDARY'];
+        const rarity = random.weightedRandom(1, rarities.length);
+
+        if(['finger', 'finger'].indexOf(slot) > -1)  stats['luck'] = 0;
+
+        const maxStats = level + rarity;
+
+        let name;
+        switch (slot) {
+            case 'finger': name = 'ring'; break;
+            case 'body': name = 'armor'; break;
+            case 'head': name = 'helmet'; break;
+            case 'hands': name = 'gloves'; break;
+            case 'legs': name = 'leggings'; break;
+            case 'weapon': name = 'sword'; break;
+            case 'offhand': name = 'shield'; break;
+        }
+
+        for(let i = 0; i <= maxStats; i++) { // levels + 1 = amount of stats point applied
+            const randomStat = random.arrayValue(Object.keys(stats));
+            stats[randomStat]++;
+            if(random.number(1, 100) === 3) {
+                const randomStat = random.arrayValue(Object.keys(stats));
+                stats[randomStat]++;
+            }
+        }
+
+        const finalStats = {};
+        let statsString = '';
+        for(let i in stats) {
+            if(stats[i] > 0) finalStats[i] = stats[i];
+            statsString += `;${i}:${stats[i]}`
+        }
+
+        const id = await itemsModel.create(characterID, name, rarity, slot, statsString, level);
+        const itemRecord = await itemsModel.get(id);
+        return new Item(itemRecord);
     }
 }
