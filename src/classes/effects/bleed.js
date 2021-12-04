912fc1f0b821446cded05c4ea73fc82afe6a1eee
RvStFyth <r.vansintfeyth@protonmail.com>
2021-12-04 17:55:10 +0100

    Stab move and bleed effect added

--- src/classes/effects/bleed.js
+++ src/classes/effects/bleed.js
@@ -1,6 +1,24 @@
+const random = require('../../helpers/random');
 const Effect = require('../effect');
 
 class Bleed extends Effect
 {
 
+    constructor(character, turns, minBleed, maxBleed) {
+        super(character, turns);
+
+        this.minBleed = minBleed;
+        this.maxBleed = maxBleed;
+    }
+
+    post()
+    {
+        const damage = random.number(this.minBleed, this.maxBleed);
+        this.character.stats.health -= damage;
+
+        super.post();
+        return `${this.character.name} got ${damage} damage from bleed..`
+    }
 }
+
+module.exports = Bleed
--- src/classes/moves/stab.js
+++ src/classes/moves/stab.js
@@ -1,3 +1,4 @@
+const Bleed = require('../effects/bleed');
 const Move = require('../move');
 
 class Stab extends Move
@@ -6,10 +7,23 @@ class Stab extends Move
 
     description = 'Causes the target too bleed for 3 turns';
 
-    constructor() {
-        super();
+    constructor(character) {
+        super(character);
 
         this.cooldown = 5;
+    }
+
+    run(targets)
+    {
+        // todo, target constitution should lower the amount a bit
+        const target = targets[0];
+        const min = Math.max(1, parseInt(parseInt(this.character.stats.strength) / 10));
+        const max = Math.max(1, parseInt(parseInt(this.character.stats.strength) / 5));
+
+        const effect = new Bleed(target, 3, min, max);
+        target.setEffect(effect);
+
+        return `${this.character.name} stabbed ${target.name}, causing bleed for 3 turns..`
     }
 }
 
